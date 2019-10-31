5ab1d08c3195d9ef73f363e76ad2687eddd84eae
pipiJua <honguita@gmail.com>
2019-10-31 14:13:03 -0300

    cheatsheet Markdown

---
+++ teoriaEnR.R
@@ -0,0 +1,15 @@
+#Tema 1 - R basis Estadistica Descriptiva
+
+#install.packages("nombre_paquete", dep = TRUE): instala o actualiza un paquete de R
+install.packages("tidyverse", dep = TRUE)
+library(tidyverse)
+install.packages("magic", dep = TRUE)
+library(magic)
+
+install.packages("abind", dep = TRUE)
+magic(6)
+
+#library(nombre_del_paquete): carga un paquete ya instalado
+installed.packages()
+
+
